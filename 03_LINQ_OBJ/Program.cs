227948684ddb4dd985bda5705f47fffac738b65a
Heber Pietrafeza <hpietrafeza@autodoc.com.br>
2019-04-08 17:59:38 -0300

    linq object e join

---
+++ 03_LINQ_OBJ/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_LINQ_OBJ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Usuario> lista = new List<Usuario>();
+            lista.Add(new Usuario() { Nome = "Jose", Email = "jose@gmail.com" });
+            lista.Add(new Usuario() { Nome = "João", Email = "joao@hotmail.com" });
+            lista.Add(new Usuario() { Nome = "Maria", Email = "maria@ig.com" });
+            lista.Add(new Usuario() { Nome = "Pedro", Email = "pedro@gmail.com" });
+            lista.Add(new Usuario() { Nome = "Heber", Email = "heber@gmail.com" });
+
+            var listaFiltrada = lista.Where(a => a.Email.Contains("gmail")).Select(a => a);
+
+            foreach (var item in listaFiltrada)
+            {
+                Console.WriteLine("Nome: {0} - Email: {1}", item.Nome, item.Email);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 04_LINQ_JOIN/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_LINQ_JOIN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listaAutor = new List<Autor>();
+            listaAutor.Add(new Autor() { Id = 1, Nome = "José" });
+            listaAutor.Add(new Autor() { Id = 2, Nome = "Maria" });
+            listaAutor.Add(new Autor() { Id = 3, Nome = "João" });
+
+            var listaLivros = new List<Livro>();
+            listaLivros.Add(new Livro() { Id = 1, AutorId = 2, Titulo = "História sem Fim"});
+            listaLivros.Add(new Livro() { Id = 2, AutorId = 2, Titulo = "Código Mestre"});
+            listaLivros.Add(new Livro() { Id = 3, AutorId = 1, Titulo = "Cubo Mágico"});
+            listaLivros.Add(new Livro() { Id = 4, AutorId = 3, Titulo = "Terror no oceano"});
+
+            var listaJoin = (from
+                            livros in listaLivros
+                            join autor in listaAutor on livros.AutorId equals autor.Id
+                            select new { livros, autor })
+                            .ToList();
+
+            foreach (var item in listaJoin)
+            {
+                Console.WriteLine("Livro: {0} - Autor: {1}", item.autor.Nome, item.livros.Titulo);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
