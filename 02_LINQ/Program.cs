88a4af102adaa50ee28dce2cddb246c6c62bd6b1
Heber Pietrafeza <hpietrafeza@autodoc.com.br>
2019-04-08 17:23:30 -0300

    sintax com lambda e sintax alternativa

--- 02_LINQ/Program.cs
+++ 02_LINQ/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _01_LINQ
+namespace _02_LINQ
 {
     class Program
     {
@@ -14,17 +14,11 @@ namespace _01_LINQ
             //LAMBDA (entrada) => (expressao)
             int[] lista = { 3, 9, 4, 6, 20, 10, 60, 90, 80, 50 };
 
-            var listaFiltrada = lista.Where(x => x > 10).Select(d => d);
-            var listaOrdenada = listaFiltrada.OrderBy(d => d);
+            //var listaFiltrada = lista.Where(x => x > 10).OrderBy(d => d).Select(d => d);
+            var listaFiltrada = from x in lista where x > 10 orderby x select x;
 
             Console.WriteLine("Lista filtrada");
             foreach (var item in listaFiltrada)
-            {
-                Console.WriteLine(item);
-            }
-
-            Console.WriteLine("Lista Ordenada");
-            foreach (var item in listaOrdenada)
             {
                 Console.WriteLine(item);
             }
